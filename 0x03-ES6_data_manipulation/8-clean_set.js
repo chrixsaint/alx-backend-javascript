0b00b263accc2edaed5b941b8f1c06c2c7cadc38
christian ochenehi peter <christianochenehipeter@gmail.com>
2024-06-03 18:29:44 +0100

    all

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,8 @@
-/**
- * Joins a set of strings with a dash.
- * @param {Set<String>} set - A collection of strings.
- * @param {String} startString - The string to strip.
- * @author Aibueku Precious Okwuoma <https://github.com/aibuekuoprecious>
- * @returns {String}
- */
 export default function cleanSet(set, startString) {
-  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
-    return '';
-  }
-
-  const filteredValues = Array.from(set).filter(value => typeof value === 'string' && value.startsWith(startString));
-  const cleanedParts = filteredValues.map(value => value.substring(startString.length)).join('-');
-  return cleanedParts;
+  let result = '';
+  if (!startString || !startString.length) return result;
+  set.forEach((i) => {
+    if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
+  });
+  return result.slice(0, result.length - 1);
 }
